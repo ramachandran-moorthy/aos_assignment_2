9b94f9e3ec56db8eb9ed6483126c3a9a0ff4928f
Ramachandran Moorthy <ramoorthy1032@gmail.com>
2024-08-28 19:24:16 +0530

    Implemented CD command

--- mainfile.cpp
+++ mainfile.cpp
@@ -1,4 +1,7 @@
 #include "PWD.h"
+#include "CD.h"
+#include <regex>
+
 
 using namespace std;
 
@@ -8,12 +11,12 @@ string getLoc(string currworkdir, string workingdir)
     if(currworkdir==workingdir)
             return "~";
         else if(currworkdir.length()>workingdir.length() && currworkdir.substr(0,workingdir.length())==workingdir)
-            return currworkdir.substr(workingdir.length());
+            return "~"+currworkdir.substr(workingdir.length());
         else
             return currworkdir;
 }
 
-void display(string workingdir)
+string display(string workingdir)
 {
     getlogin_r(buffer, 100);
     cout << buffer;
@@ -22,24 +25,52 @@ void display(string workingdir)
     getcwd(buffer, 100);
     string currworkdir = buffer;
     cout << getLoc(currworkdir, workingdir) << ">";
+    return currworkdir;
 }
 
 int main()
 {
     getcwd(buffer, 100);
-    string command, workingdir = buffer;
+    string command, currworkingdir, workingdir = buffer, prevdir="";
+    string str;
+    int val;
     do
     {
-        display(workingdir);
+        currworkingdir = display(workingdir);
         getline(cin, command);
         if(command=="pwd")
         {
-            string str = getPWD();
+            str = getPWD();
             str+="\n";
             write(1, &str[0], str.length());
             continue;
         }
-
+        regex pattern("cd .*");
+        if(regex_match(command, pattern))
+        {
+            str = command.substr(3);
+            if(str=="~")
+                val = CD(workingdir);
+            else if(str=="-")
+            {
+                if(prevdir=="")
+                {
+                    str = "Invalid Input: No previous directory record exists.\n";
+                    write(1, &str[0], str.length());
+                    continue;
+                }
+                val = CD(prevdir);
+            }
+            else
+                val = CD(str);
+            if(val!=0)
+            {
+                perror("Invalid input");
+            }
+            else
+                prevdir = currworkingdir;
+            continue;
+        }
         if(command=="exit")
             break;
     }
